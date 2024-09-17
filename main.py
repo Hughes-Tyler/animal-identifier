01db015468ad58f8058e9349278783e6f3e79793
Hughes-Tyler <tyler.j.hug@gmail.com>
2024-09-17 18:29:39 -0400

    Second commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+
+# Set the header of the app
+st.title("Machine Learning Animal Identifier")
+
+# Load the image
+image = Image.open('snowowl.jpg')
+
+# Display the image
+st.image(image, caption='What animal is this?', use_column_width=True)
+
+# Center the button using columns for layout
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    if st.button('Identify Animal'):
+        # Display "Snowy Owl" in bold and larger font size
+        st.markdown("<h2 style='text-align: center; font-weight: bold;'>Snowy Owl</h2>", unsafe_allow_html=True)
